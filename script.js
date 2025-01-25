19f2f8745707bf2792971eca6dfcef1472c739b6
SARFAS_P <139741499+SARFAS-P@users.noreply.github.com>
2025-01-25 17:35:06 +0530

    Update script3.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     const phone = document.getElementById('phone').value;
     const otp = document.getElementById('otp').value;
 
-    if (aadhar==521769012867 && phone==7306922451 && otp==344565) {
+    if (aadhar==521769012867 && phone==7306922451 && otp==123456) {
         window.location.href = 'index1.html';
     } else {
         alert('Incorrect credintials!');
